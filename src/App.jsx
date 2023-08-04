f77adc6a612e62762e76a862ec31f992cdb00f41
Tejas Naik <74360622+Tejas-Naik@users.noreply.github.com>
2023-08-04 09:43:03 +0000

    New way of implementing routes (v6.4+)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom/dist";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import Order from "./features/order/Order";
+import CreateOrder from "./features/order/CreateOrder";
+import Home from "./ui/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "/order/:orderID",
+    element: <Order />,
+  },
+]);
+
 function App() {
   const x = 12;
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App;
