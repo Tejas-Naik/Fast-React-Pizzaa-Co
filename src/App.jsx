0e873312a8225ea29b6f4c3c762833f685552767
Tejas Naik <74360622+Tejas-Naik@users.noreply.github.com>
2023-08-04 10:28:46 +0000

    Setting up AppLayout(Outlet)

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,36 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom/dist";
-import Menu from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import Order from "./features/order/Order";
+import Menu from "./features/menu/Menu";
 import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
 import Home from "./ui/Home";
+import AppLayout from "./utils/AppLayout";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/menu",
-    element: <Menu />,
-  },
-  {
-    path: "/cart",
-    element: <Cart />,
-  },
-  {
-    path: "/order/new",
-    element: <CreateOrder />,
-  },
-  {
-    path: "/order/:orderID",
-    element: <Order />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/order/:orderID",
+        element: <Order />,
+      },
+    ],
   },
 ]);
 
---
+++ src/utils/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Co.</Link>
+      <p>Tejas</p>
+    </header>
+  );
+}
+
+export default Header;
