da65bb2503a008575a41ad8bd5911d18399e6167
Tejas Naik <RNTejas2005@gmail.com>
2023-08-05 15:37:21 +0530

    Absolute positioning and making the loader better

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -38,7 +38,6 @@ function Cart() {
 
       <div>
         <Button to="/order/new">Order pizzas</Button>
-        {/* <Link to="/order/new">Order pizzas</Link> */}
         <button>Clear cart</button>
       </div>
     </div>
