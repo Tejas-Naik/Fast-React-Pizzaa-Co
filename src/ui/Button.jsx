f2802de1fde33281038d945c0de16cb9381fc3b3
Tejas Naik <RNTejas2005@gmail.com>
2023-08-05 20:43:36 +0530

    Styling the Order Form

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Button({ children, disabled, to, type }) {
     const className = "bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-6"
 
-    const base = "bg-yellow-400 uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed ";
+    const base = "text-sm bg-yellow-400 uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed ";
 
     const styles = {
         primary: base + ' px-4 py-3 md:px-6 md:py-4',
