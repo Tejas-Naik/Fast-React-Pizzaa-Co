b5d95e1bf7a20094fa59367cc484a9c59dfb39de
Tejas Naik <RNTejas2005@gmail.com>
2023-08-05 13:57:58 +0530

    Reusing Styles with React Components

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,4 +1,5 @@
-import { Link } from 'react-router-dom';
+import Button from "../../ui/Button";
+import LinkButton from '../../ui/LinkButton';
 
 const fakeCart = [
   {
@@ -29,12 +30,15 @@ function Cart() {
 
   return (
     <div>
-      <Link to="/menu" className='text-sm text-blue-500 hover:text-blue-600 hover:underline'>&larr; Back to menu</Link>
+      <LinkButton to="/menu">
+        &larr; Back to menu
+      </LinkButton>
 
       <h2>Your cart, %NAME%</h2>
 
       <div>
-        <Link to="/order/new">Order pizzas</Link>
+        <Button to="/order/new">Order pizzas</Button>
+        {/* <Link to="/order/new">Order pizzas</Link> */}
         <button>Clear cart</button>
       </div>
     </div>
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
+import Button from "../../ui/Button";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -91,15 +92,9 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting} className="bg-yellow-400 
-            uppercase font-semibold text-stone-800 py-3 px-4 inline-block
-            tracking-wide rounded-full hover:bg-yellow-300 
-            transition-colors duration-300 focus:bg-yellow-300
-            focus:outline-none focus:ring focus:ring-yellow-300 
-            focus:ring-offset-2 disabled:cursor-not-allowed"
-          >
+          <Button disabled={isSubmitting}>
             {isSubmitting ? "Placing order..." : "Order now"}
-          </button>
+          </Button>
         </div>
       </Form>
     </div>
