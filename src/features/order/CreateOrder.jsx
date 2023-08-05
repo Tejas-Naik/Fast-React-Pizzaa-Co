b6b78f816f01342bda770d9fb8f376c3953b115a
Tejas Naik <RNTejas2005@gmail.com>
2023-08-05 12:20:46 +0530

    Styling Buttons: hover, transitions, active, focus etc

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -72,15 +72,15 @@ function CreateOrder() {
             type="checkbox"
             name="priority"
             id="priority"
-            // value={withPriority}
-            // onChange={(e) => setWithPriority(e.target.checked)}
+          // value={withPriority}
+          // onChange={(e) => setWithPriority(e.target.checked)}
           />
           <label htmlFor="priority">Want to yo give your order priority?</label>
         </div>
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
+          <button disabled={isSubmitting} className="bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed">
             {isSubmitting ? "Placing order..." : "Order now"}
           </button>
         </div>
