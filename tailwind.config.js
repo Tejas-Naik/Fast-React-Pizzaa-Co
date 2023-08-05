533ad01655e199da01b4f66791018b3d641077d7
Tejas Naik <RNTejas2005@gmail.com>
2023-08-05 20:28:29 +0530

    Configuring tailwind : Custom Font Family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    // Overwrite whole fontFamily provided by tailwind
+    fontFamily: {
+      sans: 'Roboto Mono, monospace',
+    },
+
+    // Keep the values provided by tailwind and add/overwrite these
+    extend: {
+      height: {
+        screen: "100dvh"
+      }
+    },
   },
   plugins: [],
 }
